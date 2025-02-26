1496553e6aa9b4f1ff107dbf69a0faf2e2ad57ef
Patrick Gwinn <patrick.gwinn@student.swic.edu>
2025-02-26 10:20:01 -0600

    Add string formatting code

---
+++ src/strings.js
@@ -0,0 +1,9 @@
+const name = "Patrick Gwinn";
+const course = "Web Development 1";
+const expectedGrade = 95;
+
+const message =
+	name + " is taking " + course + " and expects to earn " + expectedGrade + "%";
+console.log(message);
+const message2 = `${name} is taking ${course} and expects to earn ${expectedGrade}%`;
+console.log(message2);
